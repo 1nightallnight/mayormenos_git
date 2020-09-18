742ec31c4be11d37833cdd8e6519788a57df6151
Karen Abundis <karen.abundis3673@alumnos.udg.mx>
2020-09-18 15:03:44 -0500

    Se le pregunto al usuario por su nombre y se imprimio un mensaje

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int nombre[20];
+	
+	printf("Hola, cual es tu nombre?: \n");
+	scanf("%s",&nombre);
+	printf("\nHola %s",nombre);
+	
+	
+	
+	
+	return 0;
+}
