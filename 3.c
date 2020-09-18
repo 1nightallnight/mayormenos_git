4bbb42e46af6f64940a61105855813074ae74b9e
Karen Abundis <karen.abundis3673@alumnos.udg.mx>
2020-09-18 15:16:36 -0500

    Se le pregunto al usuario por su edad y por medio de un if se define si es o no es mayor de edad, y si puede tomar

--- 3.c
+++ 3.c
@@ -10,8 +10,13 @@ int main(){
 	printf("\nHola %s",nombre);
 	printf("\n\nCuantos anios tienes %s?:",nombre);
 	scanf("%d",&edad);
-	printf("\n tienes %d anios, %s",edad,nombre);
+	printf("\nTienes %d anios",edad,nombre);
 	
+	if(edad>=18){
+		printf("\n\nEres mayor de edad %s, puedes tomar",nombre);
+	}else{
+		printf("\n\nEres menor de edad %s, no puedes tomar",nombre);
+	}
 	
 	
 	return 0;
