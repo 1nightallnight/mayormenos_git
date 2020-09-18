e6c858f5e4203da7a62846cc7f70ab9ce01186ce
Karen Abundis <karen.abundis3673@alumnos.udg.mx>
2020-09-18 15:10:58 -0500

    Le preguntamos al usuario su edad y capturamos su nombre y su edad

--- 3.c
+++ 3.c
@@ -2,12 +2,15 @@
 #include<stdlib.h>
 
 int main(){
-	int nombre[20];
+	char nombre[20];
+	int edad;
 	
 	printf("Hola, cual es tu nombre?: \n");
 	scanf("%s",&nombre);
 	printf("\nHola %s",nombre);
-	
+	printf("\n\nCuantos anios tienes %s?:",nombre);
+	scanf("%d",&edad);
+	printf("\n tienes %d anios, %s",edad,nombre);
 	
 	
 	
